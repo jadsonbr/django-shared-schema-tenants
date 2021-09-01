f2e27296dcaf22e4fb22d4b6a631b4b9be855c37
Jadson Ribeiro <jadsonbr@outlook.com.br>
2021-09-01 11:34:02 -0300

    Update __init__.py

--- shared_schema_tenants/__init__.py
+++ shared_schema_tenants/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'Django Shared Schema Tenants'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'Hugo Bessa, Samuel Bishop'
 __license__ = 'MIT License'
 __copyright__ = 'Copyright 2017 Hugo Bessa, 2019 Samuel Bishop'
